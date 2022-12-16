5e893bc9cc7a89d5e782f68cf0c105ec6bb15548
paulopariz <paulopariz01@gmail.com>
2022-12-16 20:18:07 -0300

    show pokemon

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,29 @@
   <v-app>
     <v-container>
       <v-container>
-        <v-text-field v-model="search" label="Pesquisar" placeholder="Charmander" color></v-text-field>
+        <v-text-field 
+        v-model="search" 
+        label="Pesquisar" 
+        placeholder="Charmander" 
+        color></v-text-field>
 
         {{ search }}
 
         <v-row>
-          <v-col cols="2" v-for="pokemon in filtered_pokemons" :key="pokemon.name">
+          <v-col cols="2" 
+          v-for="pokemon in filtered_pokemons" 
+          :key="pokemon.name">
+          
             <v-card @click="show_pokemon(get_id(pokemon))">
               <v-container>
                 <v-row class="mx-0 d-flex justify-center">
-                  <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${get_id(
+                  <img 
+                  :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${get_id(
                       pokemon
-                      )}.png`" :alt="pokemon.name" width="80%" />
+                      )}.png`" 
+                      :alt="pokemon.name" 
+                      width="80%" 
+                      />
                 </v-row>
                 <h2 class="text-center"> {{ get_name(pokemon) }}</h2>
               </v-container>
@@ -24,9 +35,20 @@
     </v-container>
 
 
-    <v-dialog v-model="show_dialog" width="500">
-      <v-card>
+    <v-dialog v-model="show_dialog" width="900">
+      <v-card v-if="selected_pokemon">
         <v-container>
+          <v-row>
+            <v-col cols="4">
+              <img 
+                  :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${selected_pokemon.id}.png`" 
+                      :alt="selected_pokemon.name" 
+                      width="80%" 
+                      />
+            </v-col>
+          </v-row>
+
+          {{ selected_pokemon }}
         </v-container>
       </v-card>
     </v-dialog>
@@ -65,7 +87,12 @@
       get_name(pokemon) {
         return pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
       },
-      
+      show_pokemon(id) {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) => {
+            this.selected_pokemon = response.data;
+            this.show_dialog = !this.show_dialog;
+          });
+      },
     },
  
     computed: {
