cd9dc40ffb863163e19600eb1c3332418b8d8c58
paulopariz <paulopariz01@gmail.com>
2022-12-17 01:11:14 -0300

    simple information pokemon

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,19 @@
   <v-app>
     <v-container>
       <v-container>
-        <v-text-field 
-        v-model="search" 
-        label="Pesquisar" 
-        placeholder="Charmander" 
-        color></v-text-field>
+        <v-text-field v-model="search" label="Pesquisar" placeholder="Charmander" color></v-text-field>
 
         {{ search }}
 
         <v-row>
-          <v-col cols="2" 
-          v-for="pokemon in filtered_pokemons" 
-          :key="pokemon.name">
-          
+          <v-col cols="2" v-for="pokemon in filtered_pokemons" :key="pokemon.name">
+
             <v-card @click="show_pokemon(get_id(pokemon))">
               <v-container>
                 <v-row class="mx-0 d-flex justify-center">
-                  <img 
-                  :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${get_id(
+                  <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${get_id(
                       pokemon
-                      )}.png`" 
-                      :alt="pokemon.name" 
-                      width="80%" 
-                      />
+                      )}.png`" :alt="pokemon.name" width="80%" />
                 </v-row>
                 <h2 class="text-center"> {{ get_name(pokemon) }}</h2>
               </v-container>
@@ -36,19 +26,58 @@
 
 
     <v-dialog v-model="show_dialog" width="900">
-      <v-card v-if="selected_pokemon">
+      <v-card v-if="selected_pokemon" class="px-4">
         <v-container>
-          <v-row>
+          <v-row class="d-flex align-center">
             <v-col cols="4">
-              <img 
-                  :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${selected_pokemon.id}.png`" 
-                      :alt="selected_pokemon.name" 
-                      width="80%" 
-                      />
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${selected_pokemon.id}.png`"
+                :alt="selected_pokemon.name" width="80%" />
+            </v-col>
+            <v-col cols="8">
+              <h1>{{ get_name(selected_pokemon) }}</h1>
+
+
+              <v-chip label v-for="type in selected_pokemon.types" :key="type.slot" class="mr-2">
+                {{ type.type.name }}
+              </v-chip>
+              <v-divider class="my-4"></v-divider>
+
+              <v-chip label>
+                Altura {{ selected_pokemon.height * 2.54 }} cm
+              </v-chip>
+
+              <v-chip label class="ml-2">
+                Peso {{ (selected_pokemon.weight *  0.45359237).toFixed(0) }} kgs
+              </v-chip>
+
             </v-col>
           </v-row>
 
-          {{ selected_pokemon }}
+          <h2>Moves</h2>
+
+          <v-simple-table>
+            <template>
+              <thead>
+                <tr>
+                  <th class="text-left">
+                    Level
+                  </th>
+                  <th class="text-left">
+                    Name
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr 
+                  v-for="item in filter_moves(selected_pokemon)"
+                 :key="item.move.name">
+                  <td>{{ get_move_level(item) }}</td>
+                  <td>{{ item.move.name }}</td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
         </v-container>
       </v-card>
     </v-dialog>
@@ -89,12 +118,40 @@
       },
       show_pokemon(id) {
         axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) => {
-            this.selected_pokemon = response.data;
-            this.show_dialog = !this.show_dialog;
-          });
+          this.selected_pokemon = response.data;
+          this.show_dialog = !this.show_dialog;
+        });
+      },
+      get_move_level(move){
+        for (let version of move.version_group_details){
+          if (
+            version.version_group.name == "sword-shield" && 
+            version.move_learn_method.name == "level-up"
+           ) {
+            return version.level_learned_at;
+          }
+        }
+        return 0;
+      },
+      filter_moves(pokemon){
+        return pokemon.moves.filter((item) => {
+        let include = false;
+          for (let version of item.version_group_details) {
+
+            console.log(version.version_group);
+
+            if (
+              version.version_group.name == "sword-shield" && 
+              version.move_learn_method.name == "machine"
+              ) {
+            include = true;
+          }
+        }
+        return include;
+        });
       },
     },
- 
+
     computed: {
       filtered_pokemons() {
         return this.pokemons.filter((item) => {
@@ -107,6 +164,10 @@
 
 <style>
   #app {
-    background-color: rgb(15, 11, 45);
+    background-image: linear-gradient(125deg,
+        hsl(0deg 86% 46%) 0%,
+        hsl(0deg 85% 40%) 25%,
+        hsl(0deg 84% 34%) 79%,
+        hsl(0deg 82% 28%) 100%);
   }
 </style>
